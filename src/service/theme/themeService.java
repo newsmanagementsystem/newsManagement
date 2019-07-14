884aced8f4fbfb247f000ef557b3ee37df7b6036
pitt1997 <1799695174@qq.com>
2019-07-14 20:07:17 +0800

    Update themeService.java

--- src/service/theme/themeService.java
+++ src/service/theme/themeService.java
@@ -1,8 +1,20 @@
 package service.theme;
-
 import entity.Theme;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface themeService {
-	public void addTheme(Theme theme);
-	public boolean checkThemeIfExist(String themeName);
+    //添加
+    public void addTheme(Theme theme);
+    //检查是否存在
+    public boolean checkThemeIfExist(String themeName);
+    // 获取所有主题
+    public List<Theme> findAllThemes();
+    // 更新主题
+    public int updateTheme(Theme theme);
+    // 根据名字查找主题
+    public Theme findThemeByName(String name);
+    // 通过tid删除主题
+    public int deleteTheme(int tid);
+
 }
