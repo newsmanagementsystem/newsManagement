6c5b9001822c52df72f7be49378d894dde8284aa
pitt1997 <1799695174@qq.com>
2019-07-14 20:07:39 +0800

    Update themeServiceImpl.java

--- src/service/theme/themeServiceImpl.java
+++ src/service/theme/themeServiceImpl.java
@@ -4,28 +4,63 @@ import dao.themeDao;
 import dao.themeDaoImpl;
 import entity.Theme;
 
+import java.util.List;
+
 public class themeServiceImpl implements themeService {
 
-	@Override
-	public void addTheme(Theme theme) {
-		//创建数据访问层对象
-		themeDao themeDaoObject = new themeDaoImpl();
-		
-		//执行添加主题方法
-		themeDaoObject.addTheme(theme);
-	}
-	
-	@Override
-	public boolean checkThemeIfExist(String themeName) {
-		//创建数据访问层对象
-		themeDao themeDaoObject = new themeDaoImpl();
-		
-		//用于存放判断结果
-		boolean themeIfExist = false;
-		
-		//执行查询主题方法
-		themeIfExist = themeDaoObject.checkThemeIfExist(themeName);
-		return themeIfExist;
-	}
+    @Override
+    public void addTheme(Theme theme) {
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+
+        //执行添加主题方法
+        themeDaoObject.addTheme(theme);
+    }
+
+    @Override
+    public boolean checkThemeIfExist(String themeName) {
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+
+        //用于存放判断结果
+        boolean themeIfExist = false;
+
+        //执行查询主题方法
+        themeIfExist = themeDaoObject.checkThemeIfExist(themeName);
+        return themeIfExist;
+    }
+
+
+
+    @Override
+    public List<Theme> findAllThemes() {
+
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+
+        return themeDaoObject.findAllThemes();
+    }
+
+    @Override
+    public int updateTheme(Theme theme) {
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+
+        return themeDaoObject.updateTheme(theme);
+    }
+
+    @Override
+    public Theme findThemeByName(String name) {
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+        return themeDaoObject.findThemeByName(name);
+    }
+
+    @Override
+    public int deleteTheme(int tid) {
+        //创建数据访问层对象
+        themeDao themeDaoObject = new themeDaoImpl();
+        return themeDaoObject.deleteTheme(tid);
+    }
 
 }
