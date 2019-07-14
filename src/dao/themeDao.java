7c3129225c4666f6793725812eecd4e981eded44
pitt1997 <1799695174@qq.com>
2019-07-14 20:08:28 +0800

    Update themeDao.java
    增加修改 查询所有  删除主题

--- src/dao/themeDao.java
+++ src/dao/themeDao.java
@@ -2,7 +2,17 @@ package dao;
 
 import entity.Theme;
 
+import java.util.List;
+
 public interface themeDao {
-	public void addTheme(Theme theme);
-	public boolean checkThemeIfExist(String themeName);
+    public void addTheme(Theme theme);
+    public boolean checkThemeIfExist(String themeName);
+
+    public List<Theme> findAllThemes();
+    // 更新主题
+    public int updateTheme(Theme theme);
+    // 根据名字查找主题
+    public Theme findThemeByName(String name);
+    // 通过tid删除主题
+    public int deleteTheme(int tid);
 }
